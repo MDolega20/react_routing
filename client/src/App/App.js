10fcd187fe6463418853a57823cb8eedd7ea2a46
MDolega20 <44843530+MDolega20@users.noreply.github.com>
2019-03-10 21:32:47 +0100

    initialize

---
+++ client/src/App/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Route, Switch, NavLink, Redirect } from 'react-router-dom';
+
+// import Home from './pages/Home';
+// import List from './pages/List';
+
+class Home extends Component{
+  render(){
+    return <h1>Home page</h1>;
+  }
+}
+class Page1 extends Component{
+  render(){
+    return <h1>It's working page 1</h1>;
+  }
+}
+class Page2 extends Component{
+  render(){
+    return <h1>It's working page 2</h1>;
+  }
+}
+class Page3 extends Component{
+  render(){
+    return <h1>It's working page 3</h1>;
+  }
+}
+
+class App extends Component {
+  render() {
+    const App = () => (
+      <div>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          {/* <Redirect to='page1' /> */}
+          <Route path='/page1' component={ () => <Page1 />}/>
+          <Route path='/page2' component={ () => <Page2 />}/>
+          <Route path='/page3' component={ () => <Page3 />}/>
+        </Switch>
+      </div>
+    )
+    return (
+      <>
+      <div>
+        <NavLink to='/page1'>Podstrona 1</NavLink>
+        <NavLink to='/page2'>Podstrona 2</NavLink>
+        <NavLink to='/page3'>Podstrona 3</NavLink>
+      </div>
+        <App/>
+      </>
+    );
+  }
+}
+
+export default App;
